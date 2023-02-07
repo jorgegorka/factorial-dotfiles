d71b0457bf6714dccac415c1eeb352dc65991e1b
Jorge Alvarez <jorge@alvareznavarr.es>
2023-02-07 11:42:27 +0100

    Add plugins and keymaps.

--- private_dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ private_dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,68 +1,71 @@
 require("nvim-treesitter.configs").setup({
-    ensure_installed = {
-        "bash",
-        "css",
-        "graphql",
-        "html",
-        "javascript",
-        "jsdoc",
-        "json",
-        "lua",
-        "markdown",
-        "ruby",
-        "scss",
-        "toml",
-        "typescript",
-        "yaml",
+  ensure_installed = {
+    "bash",
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "jsdoc",
+    "json",
+    "lua",
+    "markdown",
+    "ruby",
+    "scss",
+    "toml",
+    "typescript",
+    "yaml",
+  },
+  highlight = {
+    enable = true, -- false will disable the whole extension
+    additional_vim_regex_highlighting = true,
+  },
+  rainbow = {
+    enable = true
+  },
+
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<CR>",
+      scope_incremental = "<CR>",
+      node_incremental = "<TAB>",
+      node_decremental = "<S-TAB>",
     },
-    highlight = {
-        enable = true, -- false will disable the whole extension
-        additional_vim_regex_highlighting = true,
+  },
+
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+        ["ab"] = "@block.outer",
+        ["ib"] = "@block.inner",
+      },
     },
 
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            init_selection = "<CR>",
-            scope_incremental = "<CR>",
-            node_incremental = "<TAB>",
-            node_decremental = "<S-TAB>",
-        },
+    move = {
+      enable = true,
+      set_jumps = true,
+      goto_next_start = {
+        [']m'] = '@function.outer',
+        [']]'] = '@class.outer'
+      },
+      goto_next_end = {
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer'
+      },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer'
+      },
+      goto_previous_end = {
+        ['[M'] = '@function.outer',
+        ['[]'] = '@class.outer'
+      }
     },
-
-    textobjects = {
-        select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = "@class.inner",
-                ["ab"] = "@block.outer",
-                ["ib"] = "@block.inner",
-            },
-        },
-
-        move = {
-            enable = true,
-            set_jumps = true,
-            goto_next_start = {
-                [']m'] = '@function.outer',
-                [']]'] = '@class.outer'
-            },
-            goto_next_end = {
-                [']M'] = '@function.outer',
-                [']['] = '@class.outer'
-            },
-            goto_previous_start = {
-                ['[m'] = '@function.outer',
-                ['[['] = '@class.outer'
-            },
-            goto_previous_end = {
-                ['[M'] = '@function.outer',
-                ['[]'] = '@class.outer'
-            }
-        },
-    },
+  },
 })
